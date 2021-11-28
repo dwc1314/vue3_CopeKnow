ffbde642863f2ce40a33d5451bca08ca107da16f
dwc1314 <13032851215@163.com>
2021-11-28 21:39:16 +0800

    11

--- src/App.vue
+++ src/App.vue
@@ -7,13 +7,34 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import 'bootstrap/dist/css/bootstrap.min.css'
-import ColumnList, {ColumnProps} from './components/ColumnList.vue'
+import ColumnList, {ColumnProps} from '@/components/ColumnList.vue'
 const testData: ColumnProps[] = [
   {
     id: 1,
     title: 'test1专栏',
-    avatar: '这是test1专栏',
-    description: 'http://www.baidu.com'
+    description: '这是test1专栏',
+    avatar: 'http://www.baidu.com'
+  },
+  {
+    id: 2,
+    title: 'test2专栏',
+    description: '这是test2专栏',
+    avatar: '/assets/logo.png'
+  },{
+    id: 3,
+    title: 'test3专栏',
+    description: '这是test3专栏'
+    // avatar: 'http://www.baidu.com'
+  },{
+    id: 4,
+    title: 'test4专栏',
+    description: '这是test4专栏',
+    avatar: 'http://www.baidu.com'
+  },{
+    id: 5,
+    title: 'test5专栏',
+    description: '这是test5专栏',
+    avatar: 'http://www.baidu.com'
   }
 ]
   
